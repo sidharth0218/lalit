23d3d37d964c84ab1015ef4f033b9db56a676ea8
DESKTOP-J44V8MH\OS <sidharthkashyap0218@gmail.com>
2023-06-02 00:40:06 +0530

    chnge in home

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <h1>this is home page </h1>
 <h2> this is second page </h2>
 <a href="https://www.facebook.com/">click on it</a>
-<p>fdfdfd</p>
+<p>ghghfghgf</p>
 <button type="buttom"><a href="https://www.instagram.com"></a></button>
 
